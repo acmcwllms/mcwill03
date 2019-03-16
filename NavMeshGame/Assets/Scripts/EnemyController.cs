4c397af09df3760622db935876e8f3e04533d1f1
Alexandra McWilliams <acmcwllms@gmail.com>
2019-03-16 11:56:49 -0500

    NavMeshAssignment
    Finished NavMesh Assignment

---
+++ NavMeshGame/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float lookRadius = 10f; //Detection range for player
+
+    public Transform target; //Player
+    public NavMeshAgent agent1; //NavMeshAgent
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent1.SetDestination(target.position);
+
+            FaceTarget();
+        }
+    }
+}
+
